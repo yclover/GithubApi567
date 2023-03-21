5c1b5ed8b5c61af6495c9ea8cfa0a186b854727c
yclover <1031092112@qq.com>
2023-03-21 10:47:58 -0400

    Modify the code to mock the github api

--- GithubApi.py
+++ GithubApi.py
@@ -2,50 +2,22 @@
 # Course: SSW-567A
 
 import requests
-import json
-import unittest
-
-def github_api(userID):
-    # get the whole data from the specific user
-    data = requests.get("https://api.github.com/users/%s/repos" % userID)
-
-    # convert it into json
-    response = data.json()
-
-    # check invalid user or non repo
-    if data.status_code != 200:
-        print("User Not Found")
-        return False
-
-    if len(response) == 0:
-        print("Zero Repo")
-        return False
-
-    # traversal the whole data, get the repoName by the array index
-    # then get the commit in json by splitting the str to remove the symbol
-    for i in range(len(response)):
-        repoName = response[i]['name']
-        # commits_url = requests.get("https://api.github.com/users/%s/%s" % userID, repoName)
-        commits = len(requests.get(response[i]['commits_url'].split("{")[0]).json())
-        print("Repo: " + repoName + " Numbers of commits: " + str(commits))
-
-    return True
-
-# testcase
-class TestGithubApi(unittest.TestCase):
-    def testGithubApi1(self):
-        self.assertEqual(github_api('yclover'), True)
-
-    def testGithubApi2(self):
-        self.assertEqual(github_api('EmanAlOmar'), True)
-
-    def testGithubApi3(self):
-        self.assertEqual(github_api('francescaseverino'), True)
 
 
+repoURL = "https://api.github.com/users/EmanAlOmar/repos"
+commitsURL = "https://api.github.com/users/EmanAlOmar/repos/commits"
+def getApi():
+    response = requests.get(repoURL)
 
-if __name__ == '__main__':
-   unittest.main()
+    if response.ok:
+        return response
+def getCommits():
+    response = requests.get(commitsURL)
+    if response.ok:
+        return response
+    else:
+        return None
+
 
 
 
---
+++ TestGithubApi.py
@@ -0,0 +1,20 @@
+# Author: Sijie Yu
+# Course: SSW-567A
+
+from GithubApi import getApi
+from GithubApi import getCommits
+
+from unittest.mock import Mock, patch
+from nose.tools import assert_is_not_none
+
+
+@patch("GithubApi.requests.get")
+def test_getting_GithubApi(mock_get):
+    mock_get.return_value.ok = True
+    responseForApi = getApi()
+    responseForCommits = getCommits()
+
+    assert_is_not_none(responseForApi)
+    assert_is_not_none(responseForCommits)
+
+
